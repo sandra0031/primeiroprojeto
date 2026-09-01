e35a25834f9737ac64bd6cb1389757869427b48c
sandra0031 <sanopcomp@gmail.com>
2026-09-01 19:54:22 -0300

    update

---
+++ PrimeiroProjeto/PrimeiroProjeto/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PrimeiroProjeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.pnlEntrar = new System.Windows.Forms.Panel();
+            this.pnlEntrar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(196, 73);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(41, 16);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email";
+            this.lblEmail.Click += new System.EventHandler(this.lblEmail_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(53, 92);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(331, 22);
+            this.txtEmail.TabIndex = 1;
+            this.txtEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(196, 167);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(46, 16);
+            this.lblSenha.TabIndex = 2;
+            this.lblSenha.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(53, 186);
+            this.txtSenha.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(331, 22);
+            this.txtSenha.TabIndex = 3;
+            this.txtSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(145, 247);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(144, 32);
+            this.btnEntrar.TabIndex = 4;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // pnlEntrar
+            // 
+            this.pnlEntrar.Controls.Add(this.lblEmail);
+            this.pnlEntrar.Controls.Add(this.btnEntrar);
+            this.pnlEntrar.Controls.Add(this.txtEmail);
+            this.pnlEntrar.Controls.Add(this.txtSenha);
+            this.pnlEntrar.Controls.Add(this.lblSenha);
+            this.pnlEntrar.Location = new System.Drawing.Point(187, 28);
+            this.pnlEntrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlEntrar.Name = "pnlEntrar";
+            this.pnlEntrar.Size = new System.Drawing.Size(427, 393);
+            this.pnlEntrar.TabIndex = 5;
+            this.pnlEntrar.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlEntrar_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlEntrar);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(634, 432);
+            this.Name = "Form1";
+            this.Text = "Entrar";
+            this.pnlEntrar.ResumeLayout(false);
+            this.pnlEntrar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Panel pnlEntrar;
+    }
+}
+
---
+++ PrimeiroProjeto/PrimeiroProjeto/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiroProjeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Resize += (s, e) => CentralizarPainel();
+        }
+
+        private void lblEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlEntrar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void CentralizarPainel()
+        {
+            pnlEntrar.Left = (this.ClientSize.Width - pnlEntrar.Width) / 2;
+            pnlEntrar.Top = (this.ClientSize.Height - pnlEntrar.Height) / 2;
+        }
+    }
+}
