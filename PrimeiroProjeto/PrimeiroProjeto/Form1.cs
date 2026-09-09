0091414b3606eae7d2e710ffbf4ec79c855e8a81
sandra0031 <sanopcomp@gmail.com>
2026-09-08 21:45:57 -0300

    trab 3

--- PrimeiroProjeto/PrimeiroProjeto/Form1.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -57,26 +58,39 @@ namespace PrimeiroProjeto
                 return;
             }
 
-
-            if (email.ToLower() == EmailCadastrado
-                && senha == SenhaCadastrada)
+            using (MySqlConnection conexao =
+Conexao.Abrir())
             {
-                frmPrincipal principal = new frmPrincipal();
-                principal.DefinirBoasVindas(NomeCadastrado);
-
-                principal.Show();
-                this.Hide();
-            }
-
-
-            else
-            {
-                MessageBox.Show(
+                string sql =
+                "SELECT nome FROM usuarios " +
+                "WHERE email = @email " +
+                "AND senha = @senha";
+                MySqlCommand comando =
+                new MySqlCommand(
+                sql, conexao);
+                comando.Parameters.AddWithValue(
+                "@email", email);
+                comando.Parameters.AddWithValue(
+                "@senha", senha);
+                object resultado =
+                comando.ExecuteScalar(); if (resultado != null)
+                {
+                    frmPrincipal principal =
+                    new frmPrincipal();
+                    principal.DefinirBoasVindas(
+                    resultado.ToString());
+                    principal.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show(
                     "Email ou senha incorretos.",
                     "Erro", MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
-                txtSenha.Clear();
-                txtSenha.Focus();
+                    txtSenha.Clear();
+                    txtSenha.Focus();
+                }
             }
         }
 
